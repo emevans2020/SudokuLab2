c272d3fea74db711236e7ecbc27e8905a93565a1
emilyevans <emilyevans@wifi-roaming-128-4-108-158.host.udel.edu>
2018-09-18 16:47:34 -0400

    2nd commit

--- SudokuBLL/src/main/java/pkgHelper/Sudoku.java
+++ SudokuBLL/src/main/java/pkgHelper/Sudoku.java
@@ -1,5 +1,19 @@
 package pkgHelper;
 
-public class Sudoku {
+import pkgHelper.LatinSquare;
 
+public class Sudoku extends LatinSquare {
+	
+	public Sudoku () {
+		super();
+	}
+
+	public Sudoku (int [][] latinsquare) {
+		super (latinsquare);
+	}
+	
+	protected int[][] getPuzzle(){
+		return super.getLatinSquare();
+	}
+	
 }
