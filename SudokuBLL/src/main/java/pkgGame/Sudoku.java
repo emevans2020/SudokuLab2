aa6ef4d96b9802f992d4214e2937b3ca11c1a629
emilyevans <emilyevans@128.4.78.152>
2018-09-23 14:05:14 -0400

    push

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -4,6 +4,14 @@ import pkgHelper.LatinSquare;
 
 public class Sudoku extends LatinSquare {
 	
+	/**  the length of the width/height of the Sudoku puzzle.
+	 * 
+	 */
+	private int	iSize;
+	
+	private int	iSqrtSize;
+
+
 	public Sudoku () {
 		super();
 	}
@@ -16,23 +24,81 @@ public class Sudoku extends LatinSquare {
 		return super.getLatinSquare();
 	}
 	
-	protected int [] getRegion (int region) {
+	/** pass in a given region, get back a one-dimensional array 
+	 * of the region's content 
+	 * @param r
+	 * @return
+	 */
+	
+	// 4lines to figure out start and end
+	//nested for loop
+	//no more than 8 lines of code
+	protected int [] getRegion (int r) {
+		int [] reg = new int [super.getLatinSquare().length];
 		
+		int i = (r % iSqrtSize) * iSqrtSize;
+		int j = (r / iSqrtSize) * iSqrtSize;
+		
+		int iMax = i + iSqrtSize;
+		int jMax = j + iSqrtSize;
+		
+		for (;j < jMax;j++) {
+			for (i = (r % iSqrtSize) * iSqrtSize; i<iMax; i++) {
+				System.out.print(j + i);
+			}
+		}
 	}
 	
-	protected int [] getRegion (int region1, int region2) {
-		
+	protected int [] getRegion (int iCol, int iRow) {
+		int i = (iCol /iSqrtSize) + ((iRow/iSqrtSize)*iSqrtSize);
+		return getRegion (i);
 	}
 	
 	protected boolean isSudoku() {
-		
+		if ()
+		//has to be latinsquare
+		//test every region to make sure every region is found in first row
 	}
 	
+	/** return 'true' if... It's a LatinSquare 
+	 * If each region doesn't have duplicates 
+	 * If each element in the first row of the puzzle is in each region of the puzzle
+	 * At least one of the elements is a zero
+	 * @return
+	 */
 	protected boolean isPartialSudoku() {
+		int i;
 		
+		if (super.isLatinSquare() == true && super.ContainsZero() == true ) {
+			for (i = 0; i < latinsquare.length; i++) {
+				if (super.hasDuplicates(getRegion (r)) == false) {
+					if (super.hasAllValues(super.getRow(0), getRegion(r)) == true) {
+						return true;
+					}
+				}
+			}
+		}
+		else
+			return false;
 	}
 	
-	protected boolean isValueValid (int val, int val1, int val2) {
-		
+	/**test to see if a given value would 'work' for a given column / row
+	 * 
+	 * @param iCol
+	 * @param iRow
+	 * @param iValue
+	 * @return
+	 */
+	protected boolean isValueValid (int iCol, int iRow, int iValue) {
+		for (int i = 0; i <latinsquare.length; i++) {
+			for (int j = 0; j <latinsquare.length; j++ ) {
+				if (super.getColumn(j)== iValue && super.getRow(i)==iValue) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+		}
 	}
 }
